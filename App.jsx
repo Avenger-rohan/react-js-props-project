287075f6d663c8e1a819618fc9ba861cd2410d6f
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2026-06-24 03:49:38 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Card from './assets/props/card'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <Card user="Rohan Gupta" img='https://images.unsplash.com/photo-1637084576418-3f25344ccb7c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvZGluZyUyMGJveXxlbnwwfHwwfHx8MA%3D%3D'/>
+
+      <Card user="Rudra Gupta" img='https://images.unsplash.com/photo-1653118417638-738353f6af13?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGRpYW1vbmQlMjByZWR8ZW58MHx8MHx8fDA%3D' />
+
+      <Card user="Rudrakshi Gupta" img='https://images.unsplash.com/photo-1674733411409-1df4327552f9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGxpdHRsZSUyMGdpcmwlMjBjdXRlfGVufDB8fDB8fHww'/>
+   
+      <Card user="Ganesh Gupta" img='https://images.unsplash.com/photo-1624421998513-77a9ebb43d0d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGJveXxlbnwwfHwwfHx8MA%3D%3D'/>
+
+         <Card user="Shiv Gupta" img='https://images.unsplash.com/photo-1693213085235-ea6deadf8cee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fGRpYW1vbmR8ZW58MHx8MHx8fDA%3D'/>
+
+            <Card user="Krishna Gupta" img='https://images.unsplash.com/photo-1671876177064-ec29026385ab?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGRpYW1vbmQlMjByZWR8ZW58MHx8MHx8fDA%3D'/>
+
+               <Card user="Laxmi Gupta" img='https://images.unsplash.com/photo-1777838186978-0904ba1d59bc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNDd8fHxlbnwwfHx8fHw%3D'/>
+
+                  <Card user="Vishnu Gupta" img='https://media.istockphoto.com/id/95757585/photo/beautiful-glowing-blue-diamond.webp?a=1&b=1&s=612x612&w=0&k=20&c=iHEHUreTTReEtTi7CCcgLEox2J7LLDm38ATRsek_fII='/>
+                  
+    </div>
+  )
+}
+
+export default App
